1e507a6d0b7d44a8ecc70de198887fb1ff0b1213
Juan <jcobapuerto@gmail.com>
2021-03-24 23:06:41 -0500

    Fresh start

--- recursos/application_bootcamp/settings.py
+++
@@ -1,7 +0,0 @@
-TRACK_WORDS = ['covid']
-TABLE_NAME = "downloaded_tweets.csv"
-TABLE_COLUMNS = ["id_str", "created_at" , "text" , \
-            "polarity" , "subjectivity" , "user_created_at" , "user_location" , \
-            "user_description" , "user_followers_count" , "longitude" , "latitude" , \
-            "retweet_count" , "favorite_count"]
-TABLE_DTYPES  = [object, str ,str, float , float , str , str , str , int , float , float , int , int]
